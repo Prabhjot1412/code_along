e34aaacf5509f1e980a35e3cadf956d31c983450
Prabhjot1412 <prabhjot@webkorps.com>
2022-09-27 01:27:09 +0530

    2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,7 @@
 
 # Helper Module
 module ApplicationHelper
-	include Pagy::Frontend
-	
+  include Pagy::Frontend
   def provider_finder(provider)
     if provider == :google_oauth2
       OmniAuth::Utils.camelize(:google)
@@ -16,5 +15,13 @@ module ApplicationHelper
     flash[:alert] = message
     redirect_to redirecting_path
  end
-end
 
+ def markdown(text)
+  return '' if text.nil?
+
+  render_options = { hard_wrap: true, link_attributes: { target: '_blank' } }
+  extensions = { fenced_code_blocks: true, strikethrough: true, tables: true, autolink: true }
+  renderer = Redcarpet::Render::HTML.new(render_options)
+  Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+end
+end
